78e429b1c64ed5eb298171fb4e36cde000f4bf49
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2024-01-30 12:12:52 -0800

    delete user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -23,6 +23,29 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = deleteUser(id);
+    if (result === false) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send();
+    }
+  });
+
+  const deleteUser = (id) => {
+    const index = users["users_list"].findIndex((user) => user["id"] === id);
+    if (index !== -1) {
+        users["users_list"].splice(index, 1);
+        return true; // Indicate successful deletion
+    }
+    return false; // Indicate user not found
+};
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
+
 const addUser = (user) => {
     users["users_list"].push(user);
     return user;
@@ -31,7 +54,7 @@ const addUser = (user) => {
   app.post("/users", (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.send();
+    res.send(userToAdd);
   });
 
 app.listen(port, () => {
